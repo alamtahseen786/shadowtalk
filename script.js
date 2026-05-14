974409a1055cf71c1af59488125d3ab14113be11
Tahseen Alam <alamtahseen687@gmail.com>
2026-05-14 17:14:06 +0530

    fix: add validation to ensure post is not empty before submission

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ function addPost(){
 
     let text = confession.value;
 
-    
+    if(text != ""){
+
+        let post = document.createElement("p");
+
+        post.innerText = text;
+
+        posts.appendChild(post);
+
+        confession.value = "";
+    }
 }
