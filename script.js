5e56ff609becc14881885b27823843fefa756fa9
Tahseen Alam <alamtahseen687@gmail.com>
2026-05-15 19:41:33 +0530

    fix: enhance addPost function with improved code formatting and comments

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+/* =========================
+   ADD POST FUNCTION
+========================= */
+
 function addPost(){
+
+
+    /* GET TEXT FROM TEXTAREA */
 
     let text = confession.value;
 
+
+
+    /* CHECK IF TEXT IS NOT EMPTY */
+
     if(text != ""){
+
+
+
+        /* CREATE NEW PARAGRAPH */
 
         let post = document.createElement("p");
 
+
+
+        /* ADD USER TEXT INSIDE PARAGRAPH */
+
         post.innerText = text;
 
+
+
+        /* SHOW POST ON SCREEN */
+
         posts.appendChild(post);
+
+
+
+        /* CLEAR TEXTAREA */
 
         confession.value = "";
     }
